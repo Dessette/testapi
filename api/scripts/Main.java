bbfefd0914a81f0921e80bfa0b2476cfeeeb888f
Dessette <kingnosmot@gmail.com>
2024-03-17 14:20:54 +0300

    Update Main.java

--- api/scripts/Main.java
+++ api/scripts/Main.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class dataMan {
+public class Main {
     
     public static void main(String []args){
         manipulateData();
